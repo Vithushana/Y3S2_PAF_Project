3a3dd0b6da29f15dd4d7bdeca7e1de8fc1f467fa
IT22182128 <IT22182128@my.sliit.lk>
2025-05-06 22:17:01 +0530

    updated

--- Backend/src/main/java/com/example/Backend/model/User.java
+++ Backend/src/main/java/com/example/Backend/model/User.java
@@ -1,5 +1,4 @@
 package com.example.Backend.model;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
