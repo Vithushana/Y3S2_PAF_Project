60a4d9b349801ea14df64bc3ff7e10e02e475d3f
IT22182128 <IT22182128@my.sliit.lk>
2025-05-06 22:12:46 +0530

    modify the user controller

--- Backend/src/main/java/com/example/Backend/controller/UserController.java
+++ Backend/src/main/java/com/example/Backend/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin(origins = "http://localhost:5173")
