37d4cfeb6da9c007b656d1850cc7c2cdccd7fd2d
IT22182128 <IT22182128@my.sliit.lk>
2025-05-06 22:16:12 +0530

    user edited

--- Backend/src/main/java/com/example/Backend/model/User.java
+++ Backend/src/main/java/com/example/Backend/model/User.java
@@ -3,7 +3,6 @@ package com.example.Backend.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 @Document(collection = "users")
 public class User {
 
