4cca79845bd53cfc2343b431bdb3d572ba6b6830
Vithushana Nirmaleswaran <127414631+Vithushana@users.noreply.github.com>
2025-05-07 19:33:59 +0530

    Update LearningPlan.java

--- Backend/src/main/java/com/example/Backend/model/LearningPlan.java
+++ Backend/src/main/java/com/example/Backend/model/LearningPlan.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
 @Document(collection = "learning_plan")
 public class LearningPlan {
     @Id
