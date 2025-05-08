f9695eae3a6d1be6fb2af848d6d1aad83e3b8503
Harshika Maheswaran <IT22064554@my.sliit.lk>
2025-05-08 10:47:41 +0530

    notification repo

--- Backend/src/main/java/com/example/Backend/repository/NotificationRepository.java
+++ Backend/src/main/java/com/example/Backend/repository/NotificationRepository.java
@@ -1,5 +1,4 @@
 package com.example.Backend.repository;
-
 import com.example.Backend.model.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
