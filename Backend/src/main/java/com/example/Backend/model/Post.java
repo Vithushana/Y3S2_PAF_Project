4d40a5ade26fba2d08b225ed719f0d34b8171d6f
Harshika Maheswaran <IT22064554@my.sliit.lk>
2025-05-08 10:46:28 +0530

    post model

--- Backend/src/main/java/com/example/Backend/model/Post.java
+++ Backend/src/main/java/com/example/Backend/model/Post.java
@@ -2,7 +2,6 @@ package com.example.Backend.model;
 
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
